d67db0c7471c7cfb529e9c2edd4fb31ed6c227ba
Luke M <44519853+lkdm@users.noreply.github.com>
2024-05-08 10:58:10 +1000

    feat (wip): queries.

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,67 @@ fn App() -> impl IntoView {
     }
 }
 
+#[derive(Params, PartialEq)]
+struct CalculatorParams {
+    principle: usize,
+    rate: usize,
+}
+
 #[component]
 fn FormExample() -> impl IntoView {
-    let query = use_query_map();
-    let count = move || query().get("count").cloned().unwrap_or_default();
+    let query = use_query::<CalculatorParams>();
+    let principle = move || {
+        query.with(|q| {
+            q.as_ref()
+                .map(|params| params.principle)
+                .unwrap_or_default()
+        })
+    };
+
+    let rate = move || query.with(|q| q.as_ref().map(|params| params.rate).unwrap_or_default());
+
+    let calculated = move || {
+        let principle = principle();
+        let rate = rate();
+        principle * rate
+    };
+
+    // create_effect(move |_| {
+    //     set_calculated(principle * rate, principle())
+    //   // immediately prints "Value: 0" and subscribes to `a`
+    //   // log::debug!("Value: {}", a());
+    // });
 
     view! {
         <div>
             <h2>Form</h2>
             <Form method="GET" action="">
-                <input
-                    type="text"
-                    name="count"
-                    value=count
-                    oninput="this.form.requestSubmit()"
-                />
-                <input type="submit"/>
+            <fieldset>
+                <legend>Your Strategy</legend>
+                <div>
+                    <label for="principle">Principle</label>
+                    <input
+                        type="text"
+                        name="principle"
+                        value=principle
+                        inputmode="numeric"
+                        oninput="this.form.requestSubmit()"
+                    />
+                </div>
+                <div>
+                    <label for="rate">Rate</label>
+                    <input
+                        type="text"
+                        name="rate"
+                        value=rate
+                        inputmode="numeric"
+                        oninput="this.form.requestSubmit()"
+                    />
+                </div>
+            </fieldset>
             </Form>
+            <h2>Calculated</h2>
+            <p>{calculated()}</p>
         </div>
     }
 }
