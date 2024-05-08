60f97a56efa14671260e3339f83ac663c7585cb8
Luke M <44519853+lkdm@users.noreply.github.com>
2024-05-08 15:59:00 +1000

    feat: add more basic rules to numeric inputs.

--- src/main.rs
+++ src/main.rs
@@ -14,29 +14,61 @@ fn App() -> impl IntoView {
     }
 }
 
-#[derive(Params, PartialEq)]
-struct CalculatorParams {
-    principle: usize,
-    rate: usize,
+#[component]
+pub fn NumberInput(
+    handle_input: SignalSetter<Option<String>>,
+    value: Memo<Option<String>>,
+) -> impl IntoView {
+    let handle_enforce_monetary_rules = move |ev: leptos::ev::KeyboardEvent| {
+        let key = ev.key().chars().next().unwrap();
+        let mut input = event_target_value(&ev);
+
+        // Only allow one decimal point
+        if key == '.' && input.contains('.') {
+            if input.ends_with("..") {
+                input.pop();
+            }
+            ev.prevent_default();
+        }
+
+        // Only allow two digits after the decimal point
+        if input.contains('.') {
+            let parts: Vec<&str> = input.split('.').collect();
+            if parts.len() == 2 && parts[1].len() >= 2 {
+                ev.prevent_default();
+                return;
+            }
+        }
+
+        // Only allow numeric characters and the decimal point
+        if !(key.is_numeric() || key == '.') {
+            ev.prevent_default();
+        }
+    };
+    view! {
+        <input
+            type="number"
+            name="principle"
+            on:keypress=handle_enforce_monetary_rules
+            on:input=move |ev| {
+                handle_input.set(Some(event_target_value(&ev)))
+            }
+            prop:value=value
+        />
+    }
 }
 
 #[component]
 fn FormExample() -> impl IntoView {
-    let (principle, set_principle) = create_query_signal::<usize>("p");
-    let (rate, set_rate) = create_query_signal::<usize>("r");
-    let (calculated, set_calculated) = create_signal(0);
+    let (principle, set_principle) = create_query_signal::<String>("p");
+    let (rate, set_rate) = create_query_signal::<String>("r");
+    let (calculated, set_calculated) = create_signal(0.0);
 
     create_effect(move |_| {
-        let p = principle().unwrap_or(1);
-        let r = rate().unwrap_or(1);
+        let p: f64 = principle().unwrap().parse().ok().unwrap();
+        let r: f64 = rate().unwrap().parse().ok().unwrap();
         set_calculated(p * r)
     });
-
-    let handle_enforce_numeric = move |ev: leptos::ev::KeyboardEvent| {
-        if !ev.key().chars().next().unwrap().is_numeric() {
-            ev.prevent_default();
-        }
-    };
 
     view! {
         <div>
@@ -46,30 +78,11 @@ fn FormExample() -> impl IntoView {
                 <legend>Your Strategy</legend>
                 <div>
                     <label for="principle">Principle</label>
-                    <input
-                        type="number"
-                        name="principle"
-                        inputmode="numeric"
-                        on:keypress=handle_enforce_numeric
-                        on:input=move |ev| {
-                            set_principle(event_target_value(&ev).parse().ok())
-                        }
-                        prop:value=principle
-                    />
+                    <NumberInput handle_input={set_principle} value={principle}/>
                 </div>
                 <div>
                     <label for="rate">Rate</label>
-                    <input
-                        type="number"
-                        name="r"
-                        value={move || rate.get()}
-                        inputmode="numeric"
-                        on:keypress=handle_enforce_numeric
-                        on:input=move |ev| {
-                            set_rate(event_target_value(&ev).parse().ok())
-                        }
-                        prop:value=rate
-                    />
+                    <NumberInput handle_input={set_rate} value={rate}/>
                 </div>
             </fieldset>
             </Form>
